b150c73535b49778d30ce21877821d4fabf6c12c
Spilkey <spilkey1234@gmail.com>
2020-08-31 01:11:16 -0400

    user profile model

---
+++ src/models/getUserProfile.js
@@ -0,0 +1,24 @@
+class UserProfile {
+    constructor(authToken) {
+        this.authToken = authToken;
+
+      }
+    async getProfile(){
+        const response = await fetch("http://localhost:8888/login?token=" + this.authToken,
+        {
+            method: 'GET', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        });
+        let data = await response.json();
+        return data;
+    }
+}
+
+export default UserProfile;
