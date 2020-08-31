11594cc8cb8087231bb89c318cbbc49ad1ad5d75
Spilkey <spilkey1234@gmail.com>
2020-08-31 01:10:34 -0400

    Profile Component

---
+++ src/components/profile/Profile.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import './profile.css';
+
+class Profile extends Component {
+    /**
+     * 
+     * @param {Object} props profile data
+     */
+    constructor(props) {
+        super(props);
+        this.state = { profileContents: props.profile };
+    }
+    render() {
+        const profileData = this.state.profileContents;
+        let listItems;
+        if(profileData && Array.isArray(profileData)){
+            listItems = profileData.map((value, key) =>                
+                <div className="flexy-profile">
+                    <div className="profile-heading">
+                        {value.heading}
+                    </div>
+                    <div className="profile-data">
+                        {value.value}
+                    </div>
+                </div>
+            );
+        }
+        
+        return (
+            <div className="profile-top">
+                {this.props.imageDiv}
+                {listItems}
+            </div>
+        )
+    }
+}
+
+export default Profile;
