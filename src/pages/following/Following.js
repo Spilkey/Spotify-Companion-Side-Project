e0507e8138bcadf80fa47d936edc5dca21615539
Spilkey <spilkey1234@gmail.com>
2020-09-01 02:10:12 -0400

    added pagination for following page

--- src/pages/following/Following.js
+++ src/pages/following/Following.js
@@ -10,26 +10,41 @@ import './following.css'
 class Following extends Component {
     constructor(props) {
         super(props);
-        this.state = { artistData: null, dataLoaded: false }
-
+        this.state = { artistData: null, dataLoaded: false, after: null, current: '', pageNum: 1, total: null}
         this._isMounted = false;
 
-        
+        this.pageForward = this.pageForward.bind(this)
+        this.pageBack = this.pageBack.bind(this)
+        this.beforeStack = [];
     }
 
-    getFollowed(){
+    getFollowedInitial(after = null){
         if(!this.state.dataLoaded){
-            let followModel = new Followed('');
-            followModel.getFollowedArtists()
-                .then(data => this.setState({ artistData: data, dataLoaded: true }));
+            this.getFollowed(after);
         }
-
      }
 
+     getFollowed(after = ''){
+        let followModel = new Followed(after);
+        followModel.getFollowedArtists()
+            .then(data => {
+                let pageNum = this.beforeStack.length + 1;
+                let after = data.data.after;
+                let current = data.data.current;
+                let total = data.data.total;
+
+                this.setState({ artistData: data, 
+                                dataLoaded: true, 
+                                after: after, 
+                                current: current, 
+                                pageNum: pageNum, 
+                                total: total});
+            });
+     }
 
     componentDidMount() {
         this._isMounted = true;
-        this.getFollowed();
+        this.getFollowedInitial();
     }
 
     populateCards(artists) {
@@ -50,19 +65,64 @@ class Following extends Component {
         );
         return listItems;
     }
-    render() {
+    
+    makePaging(){
+        let total = this.state.total;
+        let pageTotal = Math.floor(total / 18) + 1;
+        let currentPage = this.state.pageNum;
+        let currentPageAmountString;
+        if(pageTotal > 2){
+            let lastPageAmount = (currentPage-1)*18;
+            let currentPageAmount = currentPage*18;
+            if(pageTotal !== currentPage){
+                currentPageAmountString = `Viewing ${lastPageAmount + 1} - ${currentPageAmount} of ${total}  |  Page ${currentPage} of ${pageTotal}`;
+            }else{
+                currentPageAmountString = `Viewing ${lastPageAmount + 1} - ${lastPageAmount + (total - lastPageAmount)} of ${total} |  Page ${currentPage} of ${pageTotal}`;
+            }
+        }else{
+            currentPageAmountString = `Viewing 0 - ${total}`;
+        }
+        return (
+            <div className="bottom-buttons">
+                <button onClick={this.pageBack}>&lt;</button>
+                <div>{currentPageAmountString}</div>
+                <button onClick={this.pageForward}>&gt;</button>
+            </div>
+        );
+    }
+//
+    pageForward() {
+        let after = this.state.after;
+        let current = this.state.current;
+        
+        if(after){
+            this.beforeStack.push(current);
+            this.setState({artistData: null});
+            this.getFollowed(after)
+        }
+    }
+
+    pageBack() {
+        let before = this.beforeStack.pop();
+        this.setState({artistData: null});
+        this.getFollowed(before);
+    }
+    render(){
         const dataLoaded = this.state.artistData;
         let content;
+        let paging;
         console.log(dataLoaded);
         if (dataLoaded) {
             let grid = this.makeCardGrid(dataLoaded.data.artists);
-        content = <div className="artist-grid">{grid}</div>
+            content = <div className="artist-grid">{grid}</div>
+            paging = this.makePaging();
         } else {
             content = <div class="loader">Loading...</div>
         }
         return (
             <div>
                 {content}
+                {paging}
             </div>
         )
     }
