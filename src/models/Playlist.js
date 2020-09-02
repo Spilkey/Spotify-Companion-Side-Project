5687ef16ea9937f198833d9f497f09a5d65e8044
Spilkey <spilkey1234@gmail.com>
2020-09-02 14:36:57 -0400

    boilerplate files for playlist feature

---
+++ src/models/Playlist.js
@@ -0,0 +1,6 @@
+class Playlist {
+    constructor(){
+
+    }
+    
+}
---
+++ src/pages/playlists/Playlists.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Playlists.css'
+
+class Playlists extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        
+    }
+}
+
+export default Playlists;
