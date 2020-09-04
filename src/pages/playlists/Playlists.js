af41ddb73888e8ad0efba0dd573bb6dfb91502a3
Spilkey <spilkey1234@gmail.com>
2020-09-04 00:57:25 -0400

    added code for showing playlists

--- src/pages/playlists/Playlists.js
+++ src/pages/playlists/Playlists.js
@@ -1,13 +1,90 @@
 import React, { Component } from 'react';
-import './Playlists.css'
+
+import '../../components/spinner.css';
+
+import './playlists.css'
+
+import Playlist from '../../models/Playlist'
 
 class Playlists extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
+        this.state = { playListData: null, drilledDown: false, trackData: null  };
+        this._isMounted = false;
+        this.playListModel = new Playlist();
     }
 
-    render(){
-        
+
+    getPlayLists() {
+        this.playListModel.getPlayLists()
+            .then(data => {
+                this.setState({ playListData: data});
+            });
+    }
+
+    componentDidMount() {
+        this._isMounted = true;
+        this.getPlayLists();
+    }
+
+    playListClick(playListId){
+        this.setState({drilledDown: true})
+        this.playListModel.getPlayList(playListId)            
+        .then(data => {
+            this.setState({trackData: data });
+        });
+    }
+
+    makePlayListCards(response) {
+        let cards = [];
+        let playlists = response.data;
+        playlists.forEach(element => {
+            let card = (
+                <div onClick={() => this.playListClick(element.playListId)}>
+                   <h3>{element.playListName}</h3>
+                    <div>Total Songs: {element.playListAmount}</div>
+                    <div>Owner      : {element.playListOwner}</div>
+                </div>
+            );
+            cards.push(card);
+        });
+        return cards;
+    }
+
+    makeSongCards(response){
+        console.log(response);
+    }
+    render() {
+        let response = this.state.playListData;
+        let trackData = this.state.trackData;
+        let drilledDown = this.state.drilledDown;
+        let loader = <div className="loader"></div>;
+        let content;
+        if (response) {
+            if(!drilledDown){
+                console.log(response);
+                let cards = this.makePlayListCards(response);
+                content = cards.map((value) =>
+                    <div>{value}</div>
+                )
+            }else{
+                if(trackData){
+
+                }else{
+                    content = loader;
+                }
+            }
+        } else {
+            content = loader;
+        }
+
+        return (
+
+            <div className="play-list-grid">
+                {content}
+            </div>
+        )
+
     }
 }
 
