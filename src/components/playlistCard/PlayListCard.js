45ecc41fd8e78aedcbf7909b6ac099be87a3a81d
Spilkey <spilkey1234@gmail.com>
2020-09-05 15:57:51 -0400

    added playlist card

--- src/components/playlistCard/PlayListCard.js
+++ src/components/playlistCard/PlayListCard.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 
 class PlayListCard extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
+        this.data = props.cardData || null;
     }
 
-    render(){
-        
+    render() {
+        return (
+            <div onClick={this.props.onClick}>
+                <h3>{this.data.playListName}</h3>
+                <div>Total Songs: {this.data.playListAmount}</div>
+                <div>Owner      : {this.data.playListOwner}</div>
+            </div>
+        );
     }
 }
 
-export default Playlists;
+export default PlayListCard;
