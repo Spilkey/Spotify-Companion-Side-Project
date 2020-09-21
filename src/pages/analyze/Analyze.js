b0a5def292d65e67f2735095e8aa40face355073
Spilkey <spilkey1234@gmail.com>
2020-09-21 19:52:09 -0400

    remove unnned imports

--- src/pages/analyze/Analyze.js
+++ src/pages/analyze/Analyze.js
@@ -1,12 +1,5 @@
 import React, { Component } from 'react';
 
-// Router
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
-
-import Playlist from '../../models/Playlist'
-import Storage from '../../models/Storage'
-import Tokens from '../../models/Tokens'
-
 import AnalyzePlaylist from '../../components/analyzeCard/AnalyzePlaylist'
 import AnalzyeSong from '../../components/analyzeCard/AnalyzeSong'
 
@@ -32,8 +25,8 @@ class Analyze extends Component {
         return (
             <div>
                 <div className="analyze-top">
-                    <div onClick={e => this.selectPage(1)} className="analyze-link">Analyze Playlists</div>
-                    <div onClick={e => this.selectPage(2)} className="analyze-link">Analyze Songs</div>
+                    <div onClick={e => this.selectPage(1)} logout={this.logout} className="analyze-link">Analyze Playlists</div>
+                    <div onClick={e => this.selectPage(2)} logout={this.logout} className="analyze-link">Analyze Songs</div>
                 </div>
                 {content}
             </div>
