7859e05ea100191f9a049dcfe9e66154c8640135
Spilkey <spilkey1234@gmail.com>
2020-09-25 02:42:59 -0400

    chart js components

--- src/components/analyzeCard/Visualize.js
+++ src/components/analyzeCard/Visualize.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 
-class Visualize extends Component {
+import Charter from '../../models/Charter'
+
+class VisualizeFeatures extends Component {
+    constructor(props){
+        super(props);
+        this.chartRef = React.createRef();
+
+        this.title = this.props.title || "";
+        this.dataTitle = this.props.dataTitle || "";
+    }
+
+    componentDidMount(){
+        this.charter = new Charter(this.props.data, this.title, this.dataTitle, this.chartRef.current.getContext('2d'));
+    }
     render(){
-        return <div></div>
+        let chart = (<canvas ref={this.chartRef} width="90%" height="90%" ></canvas>);
+        
+        return ( 
+            <div>
+                {chart}
+            </div>
+        )
     }
 }
 
-export default Visualize;
+export default VisualizeFeatures;
