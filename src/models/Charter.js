d9eb7cdc261e328f043256db33de976c98ce6e04
Spilkey <spilkey1234@gmail.com>
2020-09-27 03:00:44 -0400

    added edge case to binning algorithm

--- src/models/Charter.js
+++ src/models/Charter.js
@@ -3,27 +3,51 @@ import Chart from 'chart.js'
 import QuickSort from './QuickSort'
 
 class Charter {
-    constructor(data, labels, label, ctx){
-        this.config = new Config(labels, label, data);
-        let configOptions = this.config.getConfig();
+    constructor(data, axisLabels, label, ctx){
+        // this.config = new Config(labels, label, data);
+        // let configOptions = this.config.getConfig();
         let formattedData = this.formatData(data);
         this.lineChart = new Chart(ctx, {
-            type: 'bubble',
+            type: 'bar',
             data: {
+                labels: formattedData.labels,
                 datasets: [{
-                    borderColor: "#AASDFA",
-                    backgroundColor: "#HFDGHD",
-                    label: 'Scatter Dataset',
-                    data: formattedData,
+                    barPercentage: 0.5,
+                    barThickness: 15,
+                    maxBarThickness: 20,
+                    minBarLength: 2,
+                    data: formattedData.data,
                 }]
             },
             options: {
+                legend: false,
+                tooltips: false,
+                elements: {
+                    rectangle: {
+                        backgroundColor: "orange",
+                        borderColor: "black",
+                        borderWidth: 1
+                    }
+                },
+                title: {
+                    display: true,
+                    text: label
+                },
                 scales: {
+                    yAxes: [{
+                      scaleLabel: {
+                        display: true,
+                        labelString: axisLabels.y
+                      }
+                    }],
                     xAxes: [{
-                        type: 'linear',
-                        position: 'bottom'
-                    }]
-                }
+                        scaleLabel: {
+                          display: true,
+                          labelString: axisLabels.x
+                        }
+                      }],
+
+                  } 
             }
         });
     }
@@ -36,6 +60,7 @@ class Charter {
         let filteredData = sortedData;
 
         let returnArray = [];
+        let labels = [];
         // lets just do linear cause doing this in log n is more sofisticated and takes time
         let x = 0;
         let prev = filteredData[0];
@@ -43,14 +68,23 @@ class Charter {
         for (let i = 1; i < filteredData.length; i ++){
             if(prev == filteredData[i]){
                 currCount ++;
-            }else{
-                returnArray.push({x: x, y: prev, r: currCount});
+            }else {
+                returnArray.push(currCount);
+                labels.push(prev);
                 x++;
                 currCount=1;
                 prev = filteredData[i];
             }
         }
-        return returnArray;
+        // base case for last item in array
+        returnArray.push(currCount);
+        labels.push(prev);
+
+        return {
+            data: returnArray,
+            labels: labels
+        };
+
     }
 }
 
