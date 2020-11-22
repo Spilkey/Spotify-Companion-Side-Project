d83984fb028fc1f2961a5c1f4642ca87bd948d20
Spilkey <spilkey1234@gmail.com>
2020-11-22 03:04:31 -0500

    added new home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import Profile from '../../components/profile/Profile'
 class Home extends Component {
@@ -13,12 +14,46 @@ class Home extends Component {
     render() {
 
         return (
-            <div class='home-screen-main-wrapper'>
-                <div class='home-screen-main'>
-                    <div className='home-screen-content'></div>
-                    <div className='home-screen-content'></div>
-                    <div className='home-screen-content'></div>
-                    <div className='home-screen-content'></div>
+            <div className='home-screen-main-wrapper'>
+                <div className='home-screen-main'>
+                    <Link to="/following">  
+                        <div className='home-screen-content'>                  
+                                <h3>Following</h3>
+                                <div className='line'></div>
+                                <p>
+                                    Explore all the artist you follow. Click on idividual artists to learn some quick info about them. 
+                                </p>
+                            
+                        </div>
+                    </Link>
+                    <Link to="/playlists">
+                        <div className='home-screen-content'>
+                                                    
+                                <h3>Playlists</h3>
+                                <div className='line'></div>
+                                <p>
+                                    Examine any of your playlists and quickly select songs to move them, follow their artists, and move to other playlists.
+                                </p>
+                            
+                        </div>
+                    </Link>
+                    <Link to="/analyze">
+                        <div className='home-screen-content'>
+                            
+                                <h3>Analyze</h3>
+                                <div className='line'></div>
+                                <p>
+                                Analyze playlist's and songs. Select a from either playlists or songs and learn indepth knowlegde about the music.  
+                                </p>
+                            
+                        </div>
+                    </Link>
+                    <Link to="/">
+                        <div className='home-screen-content'>
+                            <h3></h3>
+                            <div className='line'></div>
+                        </div>
+                    </Link>
                 </div>
             </div>
         )
